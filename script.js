3509a209c9c6c8be65b6cf37940ed456ebe64174
dmgreen3050 <dmgreen3050@gmail.com>
2025-06-14 01:26:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const channels = [
   "https://www.youtube.com/embed/videoseries?si=ECvqzona23O3jDo7&list=PLnJVRTZlANm28rG20hiPLXHOievQ8O3Ls&enablejsapi=1&controls=0&rel=0&modestbranding=1&autoplay=1", //Christmas Movies
   "https://www.youtube.com/embed/videoseries?si=mSb725kOM5Rm_ajC&list=PLiquKSP6s-eFZj2HF0fhw41D5Argpn3_G&enablejsapi=1&controls=0&rel=0&modestbranding=1&autoplay=1", //Lifetime
   "https://www.youtube.com/embed/videoseries?si=3whAWtFxipP-ydzj&list=PL7Sv7aQs2p0V1FlyUXXbVGekKW65j5QRq&enablejsapi=1&controls=0&rel=0&modestbranding=1&autoplay=1", //Christmas Music
-  "https://www.youtube.com/embed/videoseries?si=8QsYISjNeoUTHOQl&list=PLnJVRTZlANm3L7JDiPnjIrP2zxEgbdlLJ&enablejsapi=1&controls=0&rel=0&modestbranding=1&autoplay=1" //Music
-  "https://watchseinfeld.net/"//Seinfeld
+  "https://www.youtube.com/embed/videoseries?si=8QsYISjNeoUTHOQl&list=PLnJVRTZlANm3L7JDiPnjIrP2zxEgbdlLJ&enablejsapi=1&controls=0&rel=0&modestbranding=1&autoplay=1", //Music
+  "https://watchseinfeld.net/",//Seinfeld
 "https://www.youtube.com/embed/5fnsIjeByxQ?si=tY-BH-shRz4MacpL&enablejsapi=1&controls=0&rel=0&modestbranding=1&autoplay=1"//Movies
 
 
